ab4b6fa4fe3c2400b665f7089f58df10718674cc
christopherroper <croper@alumni.nd.edu>
2018-12-06 13:47:14 -0700

    day 1

---
+++ 2019/day1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //part 1
+            // System.IO.StreamReader file = new System.IO.StreamReader(@"input.txt");
+            // string line = "";
+            // int sum = 0;
+            // while((line = file.ReadLine()) != null) {
+            //     int temp = Int32.Parse(line);
+            //     sum += temp;
+            // }
+            // file.Close(); 
+            // Console.WriteLine(sum);
+
+            //part 2
+            System.IO.StreamReader file = new System.IO.StreamReader(@"input.txt");
+            string line = "";
+            int sum = 0;
+            Dictionary<int, int> frequencyTracker = new Dictionary<int, int>();
+            List<int> frequencyChanges = new List<int>();
+            while((line = file.ReadLine()) != null) {
+                int temp = Int32.Parse(line);
+                frequencyChanges.Add(temp);
+                sum += temp;
+                if(frequencyTracker.ContainsKey(sum)) {
+                    break;
+                } else {
+                    frequencyTracker.Add(sum, sum);
+                }
+            }
+            bool notFound = true;
+            while(notFound) {
+                foreach (int value in frequencyChanges) {
+                    sum += value;
+                    if(frequencyTracker.ContainsKey(sum)) {
+                        notFound = false;
+                        break;
+                    } else {
+                        frequencyTracker.Add(sum, sum);
+                    }
+                }
+            }
+            file.Close(); 
+            Console.WriteLine(sum);
+        }
+    }
+}
